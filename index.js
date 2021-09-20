d908098f2902aa7f4db4c49f674a83d34c699b9c
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-20 12:51:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(val){
   // Write your code here!
+  let result;
+  if (val <= 400) {
+    result = "This one is on me!";
+  } else if (val >= 2000 && val < 2501) {
+    result = "I will gladly take your thirty bucks."
+  } else if (val > 2500) {
+    result = "No can do."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+  let result = location === "NYC" ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(val){
   // Write your code here!
+  let response;
+  switch (val) {
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    case 'thanks for everything' :
+      response = 'Bye.';
+  }
+  return response;
 }
